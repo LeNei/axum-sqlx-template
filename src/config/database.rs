cc4d0d7ba130b162e016e9e64783063114517fae
LeNei <leonmarc.neisskenwirth@gmail.com>
2023-09-18 09:02:42 +0200

    Update sqlx

--- src/config/database.rs
+++ src/config/database.rs
@@ -34,9 +34,8 @@ impl DatabaseSettings {
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing::log::LevelFilter::Trace);
-        options
+        let options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace)
     }
 
     pub fn get_connection_pool(&self) -> PgPool {
